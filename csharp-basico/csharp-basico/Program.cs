81b5ff046afcdf1b34639066cc9a71d50f534fa8
Viviane-Silva <viviane.leite.silva@gmail.com>
2022-05-03 19:39:12 -0300

    teste funções

--- csharp-basico/csharp-basico/Program.cs
+++ csharp-basico/csharp-basico/Program.cs
@@ -1,4 +1,45 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
 
-Console.WriteLine("Teste Vivi");
+class Program
+{
+
+    /*
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite a sua idade:");
+
+        int idade = int.Parse(Console.ReadLine());
+
+
+        if (idade >= 0 && idade <= 11)
+        {
+            Console.WriteLine("É uma criança ainda...");
+        }
+        else
+        {
+            Console.WriteLine("Hmmm, você não é uma criança");
+        }
+    }*/
+    static void Main(string[] args)
+    {
+        ExibitMsg();
+        GerarPreco(10, 0.2f);
+    }
+
+    static void ExibitMsg()
+    {
+        Console.WriteLine("Esse é um teste");
+        
+    }
+
+    //testando algumas funções com float
+    static void GerarPreco(float preco, float taxa)
+    {
+        var resultado = preco % taxa + preco;
+        Console.WriteLine("Valor com taxa atribuida: " +resultado.ToString("F"));
+    }
+}
+
+
+
